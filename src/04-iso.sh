7c4f9c31dfb581a2b3e1f399387370dd95c450a4
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2026-02-10 12:07:51 +0300

    dev/stdin fix

--- src/04-iso.sh
+++ src/04-iso.sh
@@ -9,10 +9,10 @@ function package(){
     install $DESTDIR/linux $DESTDIR/iso/linux
     install $DESTDIR/initramfs $DESTDIR/iso/initrd
     echo "insmod all_video" > $DESTDIR/iso/boot/grub/grub.cfg
-    echo "linux /linux quiet console=ttyS0" > $DESTDIR/iso/boot/grub/grub.cfg
+    echo "menuentry netinstall-combo {" > $DESTDIR/iso/boot/grub/grub.cfg
+    echo "linux /linux quiet console=ttyS0 console=tty1" >> $DESTDIR/iso/boot/grub/grub.cfg
     echo "initrd /initrd" >> $DESTDIR/iso/boot/grub/grub.cfg
-    echo "boot" >> $DESTDIR/iso/boot/grub/grub.cfg
-    grub-mkrescue -o $DESTDIR/netinstall-combo.iso --fonts="" --install-modules="linux normal fat all_video" --compress=xz --locales="" $DESTDIR/iso
-    exit 1
+    echo "}" >> $DESTDIR/iso/boot/grub/grub.cfg
+    grub-mkrescue -o $DESTDIR/netinstall-combo.iso --fonts="" --compress=xz --locales="" $DESTDIR/iso
 }
 
